8c166a7972eb74dc554f4e88957bcfd850edc372
Joevenable <binechorighteous@gmail.com>
2021-03-31 09:39:10 -0500

    Init commit

---
+++ MusicLibraryWebAPI/Controllers/MusicController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+using MusicLibraryWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public MusicController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<MusicController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MusicController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value" + id;
+        }
+
+        // POST api/<MusicController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Song song)
+        {
+            try
+            {
+                _context.Songs.Add(song);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+
+            }
+        }
+
+        // PUT api/<MusicController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
